bb3099d0e4f4b09aecc5ffa485a46ed35a161a5d
farhaan-ali-v <159417241+Farhaan-Ali@users.noreply.github.com>
2026-01-13 23:00:59 +0530

    comments added

--- new-years-eve.cpp
+++ new-years-eve.cpp
@@ -12,5 +12,21 @@ int main(){
         cout<<a;}
     return 0;}
 
+<<<<<<< HEAD
+    //so to maximize the xor, we need to take numbers such that all bits are set to 1
+    // if k=1, we take the max possible,i.e., n
+    // if k>1, we take the max possible number such that all bits are set to 1
+    // (eg. if k=2, we can take 1 and 2 to get 3(11), if k=3, we can take 1,2,3 to get 00, but we can also take 1,2,4 to get 7(111))
+    // so by this logiv, a = 1
+    // a=1*2+1=3
+    // a=3*2+1=7
+    // a=7*2+1=15
+    // a=15*2+1=31
+    // Googled to find xor properties to confirm this
+=======
     //when k=1, max number is n itself
     //when k>1, max number can be sjown as 2^m -1
+
+
+// explain the problem properly 
+>>>>>>> 6960cd0a9c3eb99e79c2f57e8f8b110691b9f704
