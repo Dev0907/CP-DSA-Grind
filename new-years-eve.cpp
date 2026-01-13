76814841eb976325c8ae5aec0ebfd243286255ee
farhaan-ali-v <159417241+Farhaan-Ali@users.noreply.github.com>
2026-01-13 22:20:30 +0530

    day-1

---
+++ new-years-eve.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int max=0;
+    if (k==1) cout<<n; else{
+        long a=1;
+        while (a<n) a=a*2+1;
+        cout<<a;}
+    return 0;}
